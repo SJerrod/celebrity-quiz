2a00ab974635032132ca241ae88a3be43d210d53
Kaila Sprague McRae <kaila.sprague@icloud.com>
2020-10-22 14:02:16 -0700

    add js question value function and if else statements dquote> dquote> Co-authored-by: Jerrod Styrk <jstyrk@citadel.edu>

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,19 @@ function totalValue (number1, number2, number3, number4) {
   return parseInt(number1) + parseInt(number2) + parseInt(number3) + parseInt(number4)
 }
 
+// function dannyDivito () {
+//   if (total === 16 || >=14) {
+//     $(".result1").show();
+//   } else if (total === 13 || >=11) {
+//     $(".result2").show(); 
+//   } else if (total === 10 || >= 8) {
+//     $(".result3").show();
+//   } else if (total === 7 || >= 4) {
+//     $(".result4").show();
+//   } else {
+//     alert("Finish the Quiz!")
+//   }
+// }
 
 // User Logic
 $(document).ready(function() {
@@ -19,6 +32,23 @@ $(document).ready(function() {
     let genre = $("input[name='genre']:checked").val();
 
     let total = totalValue (occupation, relax, food, genre);
-    console.log(total);
+    
+    $(".result1").hide();
+    $(".result2").hide();
+    $(".result3").hide();
+    $(".result4").hide();
+
+    if (total >=14) {
+      $(".result1").show();
+    } else if (total >=11) {
+      $(".result2").show(); 
+    } else if (total >= 8) {
+      $(".result3").show();
+    } else if (total >= 4) {
+      $(".result4").show();
+    } else {
+      alert("Finish the Quiz!")
+    }
+
   });
 });
